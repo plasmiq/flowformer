4808cbbe04a62424171bca40cb19c60459cea253
Krzysztof Modras <krzysztof.modras@gmail.com>
2012-10-15 00:25:17 +0200

    task view 50% complete

---
+++ app/assets/javascripts/helpers/textarea.js
@@ -0,0 +1,21 @@
+Ember.TextArea.reopen({
+  attributeBindings: ['tabindex','autofocus',"spellcheck","rows","maxChars"],
+  didInsertElement: function() {
+  	//$('textarea').autogrow();
+
+  	/* Set cursor on end */
+  	var temp;
+    temp=$('textarea').val();
+    $('textarea').val('');
+    $('textarea').focus();
+    $('textarea').val(temp);
+  },
+  keyDown: function (e) {
+  	if(e.which == 13) {
+		return false 
+	}
+  },
+  placeholder: function() {
+  	return "test"
+  }.property()
+})
--- app/assets/javascripts/views/task_view.js.coffee
+++ app/assets/javascripts/views/task_view.js.coffee
@@ -1,2 +1,8 @@
 FF.TaskView = Ember.View.extend
 	templateName: "task"
+	classNames: ["task"]
+	classNameBindings: ['background']
+
+	background: (->
+  		return @get("controller.task_type")
+  	).property()
