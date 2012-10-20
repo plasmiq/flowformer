97d86e011aea5e6bfd96262619cd7d1cece1b1e0
Krzysztof Modras <krzysztof.modras@gmail.com>
2012-10-20 23:01:13 +0200

    limit number of chars in textarea to 50

--- app/assets/javascripts/helpers/textarea.js
+++ app/assets/javascripts/helpers/textarea.js
@@ -14,5 +14,22 @@ Ember.TextArea.reopen({
   	if(e.which == 13) {
 		  return false 
     }
+  },
+  keyUp: function(e) {
+    var limit = 50;
+    var elem = $('textarea');
+    //get the current text inside the textarea  
+    var text = elem.val();  
+    //count the number of characters in the text  
+    var chars = text.length;  
+
+    //check if there are more characters then allowed  
+    if(chars > limit){  
+        //and if there are use substr to get the text before the limit  
+        var new_text = text.substr(0, limit);  
+
+        //and change the current text with the new text  
+        elem.val(new_text);  
+    }  
   }
 })
