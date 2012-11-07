f53cebe90ebab0cf6b079c0e4686c8cc33399e9b
Krzysztof Modras <krzysztof.modras@gmail.com>
2012-11-07 10:16:50 +0100

    fancybox the right way

--- app/assets/javascripts/views/info_icon_view.js
+++ app/assets/javascripts/views/info_icon_view.js
@@ -2,15 +2,27 @@ FF.InfoIconView = Em.View.extend({
 	tagName: "a",
 	classNames: ["infoicon"],
 	
-	didInsertElement: function() {
-		this.$().fancybox({
-			content: "&nbsp;",
-			afterLoad: function() {
-				$(".info").appendTo(".fancybox-inner");
-			},
-			beforeClose: function() {
-				$(".info").appendTo("#info");
+	click: function() {
+		var infoView = Em.View.create({
+			templateName: "info",
+			didInsertElement: function() {
+				//Twitter widgets Init
+				!function(d,s,id){
+					var js,fjs=d.getElementsByTagName(s)[0];			
+					js=d.createElement(s);
+					js.id=id;
+					js.src="//platform.twitter.com/widgets.js";
+					fjs.parentNode.insertBefore(js,fjs);
+				}(document,"script","twitter-wjs");
+
+				//Fancybox init
+				$.fancybox( $("#info"), {
+					beforeClose: function() {
+						infoView.destroy()
+					}
+				} )
 			}
-		})
+		});
+		infoView.appendTo("body");
 	}
 })
