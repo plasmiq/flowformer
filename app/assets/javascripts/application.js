688f1baf92095df9888e65a8ffd669c3f35327fe
Krzysztof Modras <krzysztof.modras@gmail.com>
2012-12-28 00:13:31 +0100

    merged app initialization scripts into one file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
 //= require fancybox
@@ -17,9 +5,17 @@
 //= require ember
 //= require ember-data
 //= require_self
-//= require flowformer
+//= require_tree ./lib
+//= require ./store
+//= require_tree ./models
+//= require_tree ./controllers
+//= require_tree ./views
+//= require_tree ./helpers
+//= require_tree ./templates
+//= require_tree ./routes
 FF = Ember.Application.create({
 	ready: function () {
+		this.initialize();
 		var router = FF.get("router");
 		var store = router.get("store");
 		var records = store.findAll(FF.Task).toArray();
